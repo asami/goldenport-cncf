d4c4ad57658f6f5a25fd49b17c5f00f87a371606
Asami, Tomoharu <asami@asamioffice.com>
2025-04-11 17:28:02 +0900

    Initial sources.

---
+++ src/main/scala/org/simpolemodeling/componentframework/datatype/DataType.scala
@@ -0,0 +1,35 @@
+package org.simplemodeling.componentframework.datatype
+
+import scala.util.*
+
+trait EntityId {
+  def string: String
+}
+
+case class ResourceId(id: String) {
+  require (ResourceId.validate(id), s"Bad id: $id")
+}
+
+object ResourceId {
+  def validate(id: String): Boolean = true
+
+  def create(id: String): Try[ResourceId] =
+    if (validate(id))
+      Success(ResourceId(id))
+    else
+      Failure(new IllegalArgumentException(s"Bad id: $id"))
+}
+
+case class UserId(id: String) {
+  require (UserId.validate(id), s"Bad id: $id")
+}
+
+object UserId {
+  def validate(id: String): Boolean = true
+
+  def create(id: String): Try[UserId] =
+    if (validate(id))
+      Success(UserId(id))
+    else
+      Failure(new IllegalArgumentException(s"Bad id: $id"))
+}
