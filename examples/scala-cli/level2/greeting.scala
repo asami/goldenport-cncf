b0a9dd5a342eddc440e8c8d8f8a3ff0207d33ecb
Asami, Tomoharu <asami@asamioffice.com>
2026-01-19 05:23:18 +0900

    Update example.

--- examples/scala-cli/level2/greeting.scala
+++ examples/scala-cli/level2/greeting.scala
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S scala-cli shebang
-//> using repository "https://maven.pkg.github.com/asami/maven-repository"
-//> using dep "org.goldenport:goldenport-cncf_3:0.3.2
+//> using repository "https://www.simplemodeling.org/maven"
+//> using dep "org.goldenport:goldenport-cncf_3:0.3.2"
 import org.goldenport.cncf.dsl.script.*
 
 @main def main(args: String*): Unit = run(args) { call =>
